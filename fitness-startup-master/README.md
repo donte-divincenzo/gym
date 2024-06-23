c90a20a0610c5f4556676b79af2a219246e39880
donte-divincenzo <119758498+donte-divincenzo@users.noreply.github.com>
2024-06-23 18:05:54 +0500

    Add files via upload

---
+++ fitness-startup-master/README.md
@@ -0,0 +1,6 @@
+# fitness-startup
+Fitness startup landing page
+
+Demo Link - https://fit-in-abhishek.netlify.app/
+
+
---
+++ fitness-startup-master/script/script.js
@@ -0,0 +1,39 @@
+const yearEL = document.querySelector(".year");
+const year = new Date().getFullYear();
+yearEL.textContent = year;
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
+
+//////////////////////////////////
+///// SMOOTH SCROLL FEATURE //////
+//////////////////////////////////
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
